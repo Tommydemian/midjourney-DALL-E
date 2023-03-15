4953be039cf670bffc8d711c777b88509e9572b1
Tomas <tomas@getcharly.com>
2023-03-15 14:26:07 -0300

    first setup done, Home page finished

--- client/midjourney-dalle/src/App.tsx
+++ client/midjourney-dalle/src/App.tsx
@@ -1,13 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
+
+// Components
+import { CreatePost, Home } from './pages'
+// Assets
+
+import logo from './assets/assets/logo.svg'
+
 
 function App() {
  
 
   return (
-    <div className="App">
-    <p className='text-red-600 font-semibold'>Check this out</p>      
-    </div>
+    <BrowserRouter> 
+    <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b[#e6ebf4]'>
+      <Link to='/'>
+        <img src={logo} alt="logo" className='w-28 object-contain' />
+      </Link>
+
+
+      <Link 
+      to="/create-post"
+      className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'
+      >
+        Create 
+      </Link>
+      </header>
+      <main className='sm:p-8 px-4 py-2 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)] '>
+        <Routes>
+        <Route path='/' element={<Home />}  />
+        <Route path='/create-post' element={<CreatePost />}  />
+        </Routes>
+      </main>      
+    </BrowserRouter>
   )
 }
 
