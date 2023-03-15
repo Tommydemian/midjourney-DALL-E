c0d4baa6058cc35525f2c38ce1ed6fe8bbbf986c
Tomas <tomas@getcharly.com>
2023-03-14 22:46:36 -0300

    initial commit

---
+++ client/midjourney-dalle/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+    <p className='text-red-600 font-semibold'>Check this out</p>      
+    </div>
+  )
+}
+
+export default App
